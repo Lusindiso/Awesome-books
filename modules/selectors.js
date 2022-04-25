f88ec3ab5ebcad6a57771bab3f48c21d1b22a341
lUCI <lusindisontanjana@gmail.com>
2022-04-25 14:06:48 +0200

    Add selectors

---
+++ modules/selectors.js
@@ -0,0 +1,17 @@
+// Form selectors
+const title = document.querySelector('#book-title');
+const author = document.querySelector('#book-author');
+const form = document.querySelector('#form-add-book');
+const bookList = document.querySelector('.book-list');
+
+// Navigation selectors
+const allBooks = document.querySelector('.all-books');
+const addBook = document.querySelector('.add-book');
+const contact = document.querySelector('.contact');
+const navList = document.querySelector('#list');
+const navAdd = document.querySelector('#add');
+const navContact = document.querySelector('#contact');
+
+const dateTime = document.getElementById("date-time")
+
+export {title, author, form, bookList, allBooks, addBook, contact, navList, navAdd, navContact, dateTime};
